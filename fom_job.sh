19e442e892c3e99a113286564c928db1f4c22df7
Matt Kramer <mjkramer@users.noreply.github.com>
2026-03-02 13:41:56 -0800

    Prevent kill-on-bad-exit

--- fom_job.sh
+++ fom_job.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-srun -n 4 --gpus-per-task 1 --cpus-per-task 1 fom_job.middle.sh gpu &
-srun -n 8 --cpus-per-task 1 fom_job.middle.sh cpu &
+srun -K0 -n 4 --gpus-per-task 1 --cpus-per-task 1 fom_job.middle.sh gpu &
+srun -K0 -n 8 --cpus-per-task 1 fom_job.middle.sh cpu &
 
 wait; exit
