a04657ba9a0b8c80fc9e54184476cd6e1b9dd301
Matt Kramer <mjkramer@users.noreply.github.com>
2026-03-02 13:42:13 -0800

    Extract run_lar_for_fom.sh

--- fom_job.inner.sh
+++ fom_job.inner.sh
@@ -25,16 +25,5 @@ for offset in $offsets; do
     infile=${args[0]}
     model=${args[1]}
 
-    fhicl=reco_pdvd_tpcsigproc_dnnroi_${model}_${device}.fcl
-
-    mkdir -p output logs timing
-    outfile=output/$(basename "$infile" .hdf5).$model.$device.RECO.root
-    logfile=logs/$(basename "$infile" .hdf5).$model.$device.log
-    timefile=timing/$(basename "$infile" .hdf5).$model.$device.time
-
-    for _ in $(seq $NRUNS); do
-        rm -f "$outfile"
-        /usr/bin/time -f "%P %M %E" -a -o "$timefile" \
-            lar -c "$fhicl" "$infile" -o "$outfile" &> "$logfile"
-    done
+    ./run_lar_for_fom.sh "$infile" "$model" "$device" "$NRUNS"
 done
