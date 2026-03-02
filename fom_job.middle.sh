358210e99194e288965ccbdcccd5255cdd650cfc
Matt Kramer <mjkramer@users.noreply.github.com>
2026-03-01 17:10:02 -0800

    try apptainer

--- fom_job.middle.sh
+++ fom_job.middle.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-shifter --image=fermilab/fnal-wn-sl7 --module=cvmfs,gpu -- ./fom_job.inner.sh "$@"
+# shifter --image=fermilab/fnal-wn-sl7 --module=cvmfs,gpu -- ./fom_job.inner.sh "$@"
+
+/cvmfs/oasis.opensciencegrid.org/mis/apptainer/current/bin/apptainer exec --nv -B /cvmfs,/global/cfs,/global/common,/pscratch --ipc --pid /cvmfs/singularity.opensciencegrid.org/fermilab/fnal-dev-sl7:latest /bin/bash fom_job.inner.sh "$@"
